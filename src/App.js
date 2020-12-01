1aab358df744a9fbaaca36a1d94c34b820da5bff
Jacek Jarczok <kson.eu@gmail.com>
2020-12-01 12:57:55 +0100

    Install dependencies Create DogList component Add images

--- src/App.js
+++ src/App.js
@@ -1,13 +1,49 @@
 import React, {Component} from 'react';
+import DogList from './DogList';
+import { Switch, Route} from 'react-router-dom';
+import whiskey from './images/whiskey.jpg';
+import tubby from './images/tubby.jpg';
+import hazel from './images/hazel.jpg';
 import './App.css';
 
 class App extends Component {
-  render () {
-    return (
-      <div className="App">
+  static defaultProps = {
+    dogs: [
+      {
+        name: "Whiskey",
+        age: 5,
+        src: whiskey,
+        facts: [
+          "Whiskey loves eating popcorn.",
+          "Whiskey is a terrible guard dog.",
+          "Whiskey wants to cuddle with you!"
+        ]
+      },
+      {
+        name: "Hazel",
+        age: 3,
+        src: hazel,
+        facts: [
+          "Hazel has soooo much energy!",
+          "Hazel is highly intelligent.",
+          "Hazel loves people more than dogs."
+        ]
+      },
+      {
+        name: "Tubby",
+        age: 4,
+        src: tubby,
+        facts: [
+          "Tubby is not the brightest dog",
+          "Tubby does not like walks or exercise.",
+          "Tubby loves eating food."
+        ]
+      }
+    ]
+  }
 
-      </div>
-    );
+  render () {
+    return <Route path="/dogs" render={() => <DogList dogs={this.props.dogs} />} />;
   }
 }
 
